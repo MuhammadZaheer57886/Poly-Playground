6b3f4e07b0a2559faff67407229d7dbabe75ed9f
Muhammad Talha <talhanaeem347@gmail,coom>
2023-05-07 23:56:48 +0500

    photo_profile Loading state and noInternet string

--- lib/ui/authentication/profile_info/photo_profile_screen.dart
+++ lib/ui/authentication/profile_info/photo_profile_screen.dart
@@ -37,82 +37,90 @@ class _PhotoProfileScreenState extends State<PhotoProfileScreen> {
               AppColors.i.darkBrownColor.withOpacity(0.4),
             ])),
         child: SingleChildScrollView(
-          child: !isLoading
-              ? Column(
-                  children: [
-                    SizedBox(
-                      height: size.height * 0.2,
-                    ),
-                    Text(
-                      "Profile Picture",
-                      style: TextStyle(
-                        color: AppColors.i.whiteColor,
-                        fontWeight: FontWeight.w500,
-                        fontSize: size.width * 0.05,
-                      ),
-                    ),
-
-                    Container(
-                      margin: EdgeInsets.only(top: size.height * 0.08),
-                      width: size.width * 0.4,
-                      height: size.width * 0.4,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: imageUrl.isNotEmpty
-                            ? Colors.transparent
-                            : const Color(0xffB40303),
-                        image: imageUrl.isNotEmpty
-                            ? DecorationImage(
-                                image: FileImage(File(imageUrl)),
-                                fit: BoxFit.cover,
-                              )
-                            : null,
-                      ),
-                    ),
-                    //
-                    const SizedBox(
-                      height: 40,
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        getImage();
-                      },
-                      child: Container(
-                        alignment: Alignment.center,
-                        decoration: BoxDecoration(
-                          color: AppColors.i.whiteColor,
-                          borderRadius: BorderRadius.circular(10),
+          child: Stack(
+            children: [
+              Column(
+                      children: [
+                        SizedBox(
+                          height: size.height * 0.2,
                         ),
-                        margin: EdgeInsets.symmetric(
-                          horizontal: size.width * 0.16,
-                        ),
-                        width: size.width * 0.65,
-                        height: 45,
-                        child: Text(
-                          "INSERT YOUR PHOTO",
+                        Text(
+                          "Profile Picture",
                           style: TextStyle(
-                              color: AppColors.i.blackColor,
-                              fontSize: size.width * 0.045,
-                              fontWeight: FontWeight.w500),
+                            color: AppColors.i.whiteColor,
+                            fontWeight: FontWeight.w500,
+                            fontSize: size.width * 0.05,
+                          ),
                         ),
-                      ),
+
+                        Container(
+                          margin: EdgeInsets.only(top: size.height * 0.08),
+                          width: size.width * 0.4,
+                          height: size.width * 0.4,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: imageUrl.isNotEmpty
+                                ? Colors.transparent
+                                : const Color(0xffB40303),
+                            image: imageUrl.isNotEmpty
+                                ? DecorationImage(
+                                    image: FileImage(File(imageUrl)),
+                                    fit: BoxFit.cover,
+                                  )
+                                : null,
+                          ),
+                        ),
+                        //
+                        const SizedBox(
+                          height: 40,
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            getImage();
+                          },
+                          child: Container(
+                            alignment: Alignment.center,
+                            decoration: BoxDecoration(
+                              color: AppColors.i.whiteColor,
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            margin: EdgeInsets.symmetric(
+                              horizontal: size.width * 0.16,
+                            ),
+                            width: size.width * 0.65,
+                            height: 45,
+                            child: Text(
+                              "INSERT YOUR PHOTO",
+                              style: TextStyle(
+                                  color: AppColors.i.blackColor,
+                                  fontSize: size.width * 0.045,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: size.height * 0.25,
+                        ),
+                        SimpleButton(
+                          title: "CONTINUE",
+                          onTap: () {
+                            setState(() => isLoading = true);
+                            setProfile();
+                            setState(() => isLoading = false);
+                          },
+                        ),
+                      ],
                     ),
-                    SizedBox(
-                      height: size.height * 0.25,
-                    ),
-                    SimpleButton(
-                      title: "CONTINUE",
-                      onTap: () {
-                        setState(() => isLoading = true);
-                        setProfile();
-                        setState(() => isLoading = false);
-                      },
-                    ),
-                  ],
-                )
-              : const Center(
-                  child: CircularProgressIndicator(),
-                ),
+              if (isLoading) FutureBuilder(
+                builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+                  return Container(
+                      color: Colors.white24,
+                      child: const  Center(child: CircularProgressIndicator()));
+                },
+              ),
+            ],
+          )
+
         ),
       ),
     );
