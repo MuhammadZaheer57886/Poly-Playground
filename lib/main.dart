f9dbb06b365b715ed281f4f6da22287acc4b4b74
MuhammadZaheer57886 <mza57886@gmail.com>
2023-05-07 17:33:48 +0500

    issue with friend list

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,12 @@ import 'package:poly_playground/common/custom_nav.dart';
 import 'package:poly_playground/provider/internet_provider.dart';
 import 'package:poly_playground/provider/sign_in_provider.dart';
 import 'package:poly_playground/ui/authentication/splash_screen.dart';
+import 'package:poly_playground/ui/video_calls/utils/zegokeys.dart';
 import 'package:poly_playground/utils/constants/app_strings.dart';
 import 'package:provider/provider.dart';
+import 'package:zego_uikit_prebuilt_call/zego_uikit_prebuilt_call.dart';
+import 'package:zego_uikit_signaling_plugin/zego_uikit_signaling_plugin.dart';
+import 'common/store.dart';
 import 'firebase_options.dart';
 import 'ui/payment/stripe_key.dart';
 
@@ -25,12 +29,36 @@ Future<void> main() async {
     name: AppStrings.i.appName,
 
   );
+  ZegoUIKitPrebuiltCallInvitationService().setNavigatorKey(navigatorKey);
   print(result);
-  runApp(const MyApp());
+  ZegoUIKit().initLog().then((value) {
+    ///  Call the `useSystemCallingUI` method
+    ZegoUIKitPrebuiltCallInvitationService().useSystemCallingUI(
+      [ZegoUIKitSignalingPlugin()],
+    );
+
+    runApp(const MyApp());
+  });
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+
+    if (Store().userData.uid.isNotEmpty) {
+      onUserLogin();
+    }else{
+      ZegoUIKitPrebuiltCallInvitationService().uninit();
+    }
+  }
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -52,4 +80,16 @@ class MyApp extends StatelessWidget {
       ),
     );
   }
+  void onUserLogin() {
+  /// 1.2.1. initialized ZegoUIKitPrebuiltCallInvitationService
+  /// when app's user is logged in or re-logged in
+  /// We recommend calling this method as soon as the user logs in to your app.
+  ZegoUIKitPrebuiltCallInvitationService().init(
+    appID: ZegoConfig.appID ,
+    appSign: ZegoConfig.appSign ,
+    userID: Store().userData.uid,
+      userName: Store().userData.fullName,
+    plugins: [ZegoUIKitSignalingPlugin()],
+  );
+}
 }
